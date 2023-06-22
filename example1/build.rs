ed0e2b274b763d0cd4eec03808c4d5e42ba18dfe
Robert Forsman <bob.forsman@ericsson.com>
2023-06-22 15:56:28 -0400

    replace hardcoded openxr library path with an environment variable

--- example1/build.rs
+++ example1/build.rs
@@ -1,3 +1,31 @@
+use std::fmt::Write;
+
 pub fn main() {
-    println!("cargo:rustc-link-search=/home/thoth/src/ovr_openxr_mobile_sdk/OpenXR/Libs/Android/arm64-v8a/Debug");
+    let openxr_libdir = match std::env::var("OPENXR_LIBDIR") {
+        Ok(dir) => dir,
+        Err(_) => {
+            if false {
+                dump_env_variables();
+            }
+            const EXAMPLE: &str =
+                "ovr_openxr_mobile_sdk/OpenXR/Libs/Android/arm64-v8a/Debug/libopenxr_loader.so?";
+            panic!(
+                "missing OPENXR_LIBDIR environment variable, what directory contains {}?",
+                EXAMPLE
+            )
+        }
+    };
+    println!("cargo:rustc-link-search={}", openxr_libdir);
+}
+
+fn dump_env_variables() {
+    // let mut buffer = File::create("/tmp/build.txt").unwrap();
+    let mut buffer = String::new();
+    for (key, val) in std::env::vars() {
+        if val.contains("64") {
+            writeln!(&mut buffer, " key {}={}", key, val).unwrap();
+        }
+    }
+
+    panic!("{}", buffer);
 }
