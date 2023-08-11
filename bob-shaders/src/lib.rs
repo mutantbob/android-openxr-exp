c7ce3af74db78e31cbd9188b4d8d7f67730afaa6
Robert Forsman <bob.forsman@ericsson.com>
2023-08-11 09:48:28 -0400

    impl GeometryBuffer for VertexBufferBundle

--- bob-shaders/src/lib.rs
+++ bob-shaders/src/lib.rs
@@ -1,4 +1,4 @@
-use gl_thin::gl_fancy::{BoundBuffers, GPUState};
+use gl_thin::gl_fancy::{BoundBuffers, GPUState, VertexBufferBundle};
 
 pub mod flat_color_shader;
 pub mod geometry;
@@ -9,4 +9,15 @@ pub mod sun_phong_shader;
 pub trait GeometryBuffer<AT, IT> {
     fn activate<'a>(&'a self, gpu_state: &'a mut GPUState) -> BoundBuffers<'a, AT, IT>;
     fn deactivate(&self, bound_buffers: BoundBuffers<AT, IT>);
+}
+
+impl<AT,IT> GeometryBuffer<AT,IT> for VertexBufferBundle<'_, AT,IT>
+{
+    fn activate<'a>(&'a self, gpu_state: &'a mut GPUState) -> BoundBuffers<'a, AT, IT> {
+        self.bind(gpu_state).expect("failed to bind VertexBufferBundle")
+    }
+
+    fn deactivate(&self, bound_buffers: BoundBuffers<AT, IT>) {
+drop(bound_buffers)
+    }
 }
