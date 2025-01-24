62a0a78445123bef72000997e1e57643cc3d7da9
Robert Forsman <git@thoth.purplefrog.com>
2025-01-24 15:10:34 -0500

    switch over to the winit ApplicationHandler trait

--- example1/src/lib.rs
+++ example1/src/lib.rs
@@ -8,9 +8,11 @@ use drawcore::ActiveRenderer;
 use gl_thin::gl_helper::initialize_gl_using_egli;
 use std::ops::Add;
 use std::time::{Duration, Instant};
-use winit::event::{Event, WindowEvent};
+use winit::application::ApplicationHandler;
+use winit::event::{StartCause, WindowEvent};
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop, EventLoopBuilder};
 use winit::platform::android::EventLoopBuilderExtAndroid;
+use winit::window::WindowId;
 
 pub mod drawcore;
 pub mod rainbow_triangle;
@@ -36,6 +38,56 @@ pub enum AppState<T: Drawable> {
 impl<T: Drawable> Default for AppState<T> {
     fn default() -> Self {
         Self::Paused
+    }
+}
+
+pub struct MyApp<T: Drawable, F, E: std::fmt::Debug>
+where
+    F: Fn(&ActiveEventLoop) -> Result<T, E>,
+{
+    state: AppState<T>,
+    factory: F,
+}
+
+impl<T: Drawable, F, E: std::fmt::Debug> ApplicationHandler for MyApp<T, F, E>
+where
+    F: Fn(&ActiveEventLoop) -> Result<T, E>,
+{
+    fn new_events(&mut self, _event_loop: &ActiveEventLoop, _cause: StartCause) {
+        if let AppState::Active(app) = &mut self.state {
+            app.handle_events_and_draw();
+        }
+    }
+
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        match (self.factory)(event_loop) {
+            Ok(x) => {
+                self.state = AppState::Active(x);
+            }
+            Err(e) => {
+                log::error!("malfunction building drawable {:?}", e)
+            }
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        let control_flow = window_event_loop_one_pass(event, event_loop, &mut self.state);
+        event_loop.set_control_flow(control_flow);
+    }
+
+    fn suspended(&mut self, _event_loop: &ActiveEventLoop) {
+        log::debug!("suspend");
+        if let AppState::Active(app) = &mut self.state {
+            app.suspend();
+        }
+        // log::trace!("Suspended, dropping surface state...");
+        // app.surface_state = None;
+        self.state = AppState::Paused;
     }
 }
 
@@ -85,64 +137,6 @@ fn window_event_loop_one_pass<T: Drawable>(
 
 //
 
-fn event_loop_one_pass<T: Drawable, X: std::fmt::Debug, E: std::fmt::Debug>(
-    event: Event<X>,
-    event_loop: &ActiveEventLoop,
-    // control_flow: &mut ControlFlow,
-    app: &mut AppState<T>,
-    factory: impl Fn(&ActiveEventLoop) -> Result<T, E>,
-) {
-    log::trace!("Received Winit event: {event:?}");
-
-    let static_graphics = false;
-
-    let mut control_flow = match app {
-        AppState::Paused => ControlFlow::Wait,
-        AppState::Active(_) => {
-            if static_graphics {
-                ControlFlow::Poll
-            } else {
-                // trigger redraws every 6 milliseconds
-                ControlFlow::WaitUntil(Instant::now().add(Duration::from_millis(6)))
-            }
-        }
-    };
-
-    match event {
-        Event::Resumed => {
-            log::debug!("resume");
-            match factory(event_loop) {
-                Ok(x) => {
-                    *app = AppState::Active(x);
-                }
-                Err(e) => {
-                    log::error!("malfunction building drawable {:?}", e)
-                }
-            }
-        }
-        Event::Suspended => {
-            log::debug!("suspend");
-            if let AppState::Active(app) = app {
-                app.suspend();
-            }
-            // log::trace!("Suspended, dropping surface state...");
-            // app.surface_state = None;
-            *app = AppState::Paused;
-        }
-        Event::WindowEvent { event: we, .. } => {
-            control_flow = window_event_loop_one_pass(we, event_loop, app);
-        }
-        Event::NewEvents(_) => {
-            if let AppState::Active(app) = app {
-                app.handle_events_and_draw();
-            }
-        }
-        _ => {}
-    }
-
-    event_loop.set_control_flow(control_flow);
-}
-
 //#[cfg(target_os = "android")]
 #[no_mangle]
 fn android_main(android_app: AndroidApp) {
@@ -161,14 +155,14 @@ fn android_main(android_app: AndroidApp) {
 
     log::debug!("got event loop");
 
-    let mut app = AppState::<ActiveRenderer>::default();
-    event_loop
-        .run(move |evt, e_loop| {
-            event_loop_one_pass(evt, e_loop, &mut app, |event_loop| {
-                initialize_gl_using_egli();
+    let app = AppState::<ActiveRenderer>::default();
+    let mut app = MyApp {
+        state: app,
+        factory: |event_loop| {
+            initialize_gl_using_egli();
 
-                ActiveRenderer::new(event_loop)
-            })
-        })
-        .unwrap();
+            ActiveRenderer::new(event_loop)
+        },
+    };
+    event_loop.run_app(&mut app).unwrap();
 }
